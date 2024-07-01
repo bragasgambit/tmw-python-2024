45b24ec55c6a80763816f2cdf4dfac11f3a72afc
Yuri Braga <yurirenanbs@gmail.com>
2024-06-30 22:53:32 -0300

    falta o ex 1.6 para casa e o do sorvete

---
+++ dia02/ex_casa/1_4.py
@@ -0,0 +1,7 @@
+a = float(input("Digite um número: "))
+b = float(input("Digite outro número: "))
+
+soma = round(a + b,2)
+
+print("A soma de", a, "com", b, "resulta em", soma)
+print(soma)
---
+++ dia02/ex_casa/1_6.py
@@ -0,0 +1,8 @@
+segundos = int(input("Digite um número em segundos: "))
+
+minutos = segundos / 60
+horas = segundos / 3600
+
+print(horas + ":" + minutos)
+
+#incompleto
